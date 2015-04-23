0840c71d43d695444dcdb643f09da3668d2bdb6b
RKVS Raman <rkvsraman@gmail.com>
2015-04-23 19:18:36 +0530

    Added show pages in trail in Saved tav

--- src/browser_action/js/SavedCtrl.js
+++ src/browser_action/js/SavedCtrl.js
@@ -10,20 +10,27 @@ pathfinder.controller('SavedCtrl',
         function getSavedTabs() {
             $scope.no_tabs = false;
 
-            chrome.runtime.sendMessage({request: 'getSavedTabs'}, function (response) {
+            chrome.runtime.sendMessage({
+                request: 'getSavedTabs'
+            }, function (response) {
 
 
             });
         }
 
         $scope.openTab = function (id) {
-            chrome.runtime.sendMessage({request: 'openSavedTab', id: id}, function (response) {
-            });
+            chrome.runtime.sendMessage({
+                request: 'openSavedTab',
+                id: id
+            }, function (response) {});
 
         }
 
         $scope.deleteTab = function (id) {
-            chrome.runtime.sendMessage({request: 'deleteSavedTab', id: id}, function (response) {
+            chrome.runtime.sendMessage({
+                request: 'deleteSavedTab',
+                id: id
+            }, function (response) {
 
                 getSavedTabs();
             });
@@ -33,12 +40,43 @@ pathfinder.controller('SavedCtrl',
             if (message.request === 'savedTabresults') {
 
                 $scope.$apply(function () {
-                    $scope.tabs = message.results;
-                    console.log('Saved Tabs %j',$scope.tabs);
-                    if(message.results.length == 0)
-                    $scope.no_tabs = true;
+
+                    $scope.tabs = [];
+                    if (message.results.length == 0) {
+                        $scope.no_tabs = true;
+                        return;
+                    } else {
+
+                        var responses = message.results;
+                        for (var i = 0; i < responses.length; i++) {
+
+                            var tab = {};
+                            tab.id = responses[i].id;
+                            tab.note = responses[i].note;
+                            tab.creation_time = responses[i].creation_time;
+                            tab.tags = responses[i].tags;
+                            tab.no_of_pages = responses[i].no_of_pages;
+                            tab.s_pages = [];
+                            if (responses[i].nodes && responses[i].nodes.graph && responses[i].nodes.graph._nodes) {
+                                var nodes = responses[i].nodes.graph._nodes;
+                                for (var node in nodes) {
+                                    var page = {};
+                                    page.link = node;
+                                    page.title = nodes[node].title;
+                                    tab.s_pages.push(page);
+
+                                }
+                            }
+                            $scope.tabs.push(tab);
+                        }
+                    }
+                    // $scope.tabs = message.results;
+                    console.log('Saved Tabs %j', $scope.tabs);
+
                 });
-                response({success: true});
+                response({
+                    success: true
+                });
             }
         });
         getSavedTabs();
